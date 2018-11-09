0544ae2efb230ddaa66c6ff17c5552d6f3c2e097
ConnorBrereton <breretonconnor@gmail.com>
2018-11-09 21:15:22 +0000

    Fixed header to follow Betty style guide

--- shell.h
+++ shell.h
@@ -1,6 +1,22 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+/**
+ * struct commands - Structure of each node
+ *
+ * @prev_valid: Check if previous command
+ * was successful
+ *
+ * @separator: Used for character that
+ * that separates each command
+ *
+ * @command: Points to the first char
+ * in the stream
+ *
+ * @next: Address of next node
+ *
+ */
+
 typedef struct commands
 {
 	int prev_valid;
