2d8bb60514ac259033d72e41877973c35611f6d3
BennettDixon <bennettdixon16@gmail.com>
2018-11-12 23:14:10 +0000

    Added logic for our Command Queue to string parsing function to seperate string into queue of commands, need to copy data from tokens instead of only their pointers in get_command_tokens()

--- string_parser.c
+++ string_parser.c
@@ -1,5 +1,8 @@
 #include "shell.h"
+#define LOGIC_DELIMS "&|;"
 
+int is_logic(char *token);
+char **get_command_tokens(char **raw_tokens, int beg_ind, int end_ind);
 /**
  * parse_string - Writes tokens to a linked list
  *
@@ -8,30 +11,110 @@
  * Return: NULL (failure) head (success)
  */
 
-command_t *parse_string(char *str)
+queue_t *parse_string(char *str)
 {
-	char **raw_tokens;
+	char **raw_tokens, **sub_tokens;
+	int beg_ind = tok_ind = 0, end_ind = sep_ind = -1;
 
-	command_t *head = NULL;
-
+	queue_t *q = NULL;
+	
 	if (!str)
 		return (NULL);
 
+	q = create_queue();
+	if (!q)
+		return (NULL);
 	raw_tokens = strtow(str);
 
 	if (!raw_tokens)
 		return (NULL);
 
-	/* currently writes everything to one node*/
-	/* one command functionality */
-
-	head = create_command('\0', raw_tokens);
-
-	if (!head)
+	/* does not deal with logical seperators combined with words e.g: */
+	/* echo cat&&echo test, echo cat&& echo test, etc... */
+	while (raw_tokens[tok_ind]) /* NULL terminated array of tokens */
 	{
-		free_token_list(raw_tokens);
-		return (NULL);
+		if (is_logic(raw_tokens[tok_ind + 1]))
+		{
+			/* create new node */
+			end_ind = tok_ind; /* token before logic token */
+			sub_tokens = get_command_tokens(raw_tokens, beg_ind, end_ind);
+			if (!sub_tokens)
+			{
+				free_token_list(raw_tokens);
+				free_command_queue(q);
+				return (NULL);
+			}
+			seperator = (sep_ind >= 0) ? *(raw_tokens[sep_ind]) : '\0';
+			if (!enqueue(q, seperator, sub_tokens)) /* insert fail */
+			{
+				/* free everything & return NULL */
+				free_token_list(sub_tokens);
+				free_token_list(raw_tokens);
+				free_command_queue(q);
+				return (NULL);
+			}
+			sep_ind = tok_ind + 1; /* found seperator */
+			beg_ind = sep_ind + 1; /* beg command index moves forward */
+		}
+		tok_ind++;
 	}
+	free_token_list(raw_tokens);
+	return (q);
+}
 
-	return (head);
+/**
+ * is_logic - checks if current token is a logical seperator token
+ * @token: token to check
+ *
+ * Return: 1 if it is a logical seperator, 0 if not, -1 if not only sep e.g:
+ * &&echo, doesn't account for words before delim, will just return 0, e.g:
+ * cat&&echo, cat&&, will return 0.
+ */
+int is_logic(char *token)
+{
+	if (!token) /* NULL logical seperator, aka end of input */
+		return (1);
+	else if (*token == ';')
+	{
+		if (*(token + 1) == '\0')
+			return (1);
+		return (-1);
+	} /* will not be run if sep is ';' */
+	else if (is_delim(*token, LOGIC_DELIMS))
+	{
+		else if (is_delim(*(token + 1), LOGIC_DELIMS))
+		{
+			if (*(token + 2) == '\0') /* only contains delim and NULL */
+				return (1);
+			return (-1) /* contains extra characters */
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
+
+/**
+ * get_command_tokens - gets the command tokens given raw and beg to end pos's
+ * @raw_tokens: array of raw tokens (contains logical tokens like &&, ||, ;
+ * @beg_ind: index in raw_tokens of beginning of command, e.g: echo
+ * @end_ind: index in raw_tokens of end of command, e.g: cat
+ *
+ * Return: Pointer to new NULL terminated array of tokens, removing logical sep
+ */
+char **get_command_tokens(char **raw_tokens, int beg_ind, int end_ind)
+{
+	char **com_tokens = NULL;
+	int com_ind = 0;
+
+		/* +1 account for index starting at 0, +1 for NULL too */
+	com_tokens = malloc(sizeof(char *) * (((end_ind + 1) - beg_ind) + 1));
+	if (!com_tokens)
+		return (NULL);
+	while (beg_ind <= end_ind)
+	{ /* copy the actual pointers. NOTE not creating new copy of data. */
+		com_tokens[com_ind++] = raw_tokens[beg_ind++];
+	}
+	com_tokens[com_ind] = NULL; /* NULL terminate the array */
+	return (com_tokens);
 }
