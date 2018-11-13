53b7ab7b5dc58e174aac49eebe77c8c0a50b7572
BennettDixon <bennettdixon16@gmail.com>
2018-11-13 22:36:44 +0000

    Betty-style fixed documentation

--- print_errors.c
+++ print_errors.c
@@ -1,6 +1,11 @@
 #include "shell.h"
 #include <stdio.h>
 
+/**
+ * print_no_file_error - prints the no file error message to STDOUT
+ *
+ * Return: always void.
+ */
 void print_no_file_error(void)
 {
 	write(STDOUT_FILENO, "No such file or directory\n", 26);
